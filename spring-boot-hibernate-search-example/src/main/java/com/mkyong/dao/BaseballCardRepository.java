503df3a11edbc263658f6ec7240a96f7363f83a6
aundhi007 <aundhi007@gmail.com>
2018-02-18 22:58:20 +0530

    f2

--- spring-boot-hibernate-search-example/src/main/java/com/mkyong/dao/BaseballCardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mkyong.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.mkyong.model.BaseballCard;
-
-public interface BaseballCardRepository extends CrudRepository<BaseballCard,Long> {
-
-}
